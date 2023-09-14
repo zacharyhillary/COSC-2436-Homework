a769250515a77b70351e1df3ea539f7338ae0aea
zacharyhillary <111765105+zacharyhillary@users.noreply.github.com>
2023-09-14 16:11:15 -0500

    lab4almostdone

---
+++ lab4/lab4/lab4.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
+class Node {
+public:
+    int content;
+    Node* right = nullptr;
+    Node* down = nullptr;
+
+};
+
+class LL {
+public:
+    Node* head = nullptr;
+    void ReadFromFile(string inputFileString) {
+        string currentLineString;
+        fstream inputfile;
+        inputfile.open(inputFileString);
+        Node* previousRowHead = nullptr;
+        while (getline(inputfile, currentLineString)) {
+            stringstream oss(currentLineString);
+            int currentInt;
+            Node* currentNode=nullptr;
+            Node* previousNode = nullptr;
+            Node* rowHead=nullptr;
+            
+            int i = 0;
+            while (oss >> currentInt) {
+                currentNode = new Node;
+                if (head == nullptr)head = currentNode;
+                if (rowHead == nullptr)rowHead = currentNode;
+                currentNode->content = currentInt;
+                if(previousNode!=nullptr)previousNode->right = currentNode;
+                i++;
+                previousNode = currentNode;
+            }
+            Node* temp1 = previousRowHead;
+            Node* temp2 = rowHead;
+            if (temp1 != nullptr)for (int k = 0;k < i; k++) {
+                temp1->down = temp2;
+                temp1 = temp1->right;
+                temp2 = temp2->right;
+
+            }
+            previousRowHead = rowHead;
+        }
+    }
+};
+
+int main(int argc, char* argv[]) {
+    if (2 == 2) {
+        LL MatrixA;
+        //string filename=argv[1];
+        MatrixA.ReadFromFile("in1.txt");
+        cout << "done!";
+    }
+    else cout << "ERROR!! incorrect number of command line arguments!!!!";
+}

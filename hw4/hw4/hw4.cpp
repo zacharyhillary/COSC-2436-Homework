b58d0562fddbda397ae954274bc49aa5108f70e8
zacharyhillary <zacharyhillary1@gmail.com>
2023-10-31 10:54:52 -0500

    hw4 done
    hw4 done

--- hw4/hw4/hw4.cpp
+++ hw4/hw4/hw4.cpp
@@ -2,59 +2,77 @@
 //
 
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-void Hanoi3(int disk, char fromPin, char auxiliaryPin, char toPin){
+void Hanoi3(int disk, ofstream& outputfile, char fromPin, char auxiliaryPin, char toPin){
     if (disk != 0) {
 
-        Hanoi3(disk - 1, fromPin, toPin, auxiliaryPin);
-        cout << disk << " " << fromPin << " " << toPin << endl;
-        Hanoi3(disk - 1, auxiliaryPin, fromPin, toPin);
+        Hanoi3(disk - 1, outputfile, fromPin, toPin, auxiliaryPin);
+        outputfile << disk << " " << fromPin << " " << toPin << endl;
+        Hanoi3(disk - 1,  outputfile, auxiliaryPin, fromPin, toPin);
     }
 }
-void Hanoi4(int disk, char fromPin, char auxiliaryPin1, char auxiliaryPin2, char toPin) {
+void Hanoi4(int disk, ofstream& outputfile, char fromPin, char auxiliaryPin1, char auxiliaryPin2, char toPin) {
     if(disk!= 0) {
-        if(disk == 1) cout << disk << " " << fromPin << " " << toPin << endl;
+        if(disk == 1) outputfile << disk << " " << fromPin << " " << toPin << endl;
         else {
-            Hanoi4(disk - 2, fromPin, auxiliaryPin2, toPin, auxiliaryPin1);
+            Hanoi4(disk - 2,  outputfile, fromPin, auxiliaryPin2, toPin, auxiliaryPin1);
 
-            cout <<disk- 1 << " " << fromPin << " " << auxiliaryPin2<<endl;
-            cout <<disk<< " " << fromPin << " " << toPin<<endl;
-            cout <<disk- 1 << " " << auxiliaryPin2 << " " << toPin<<endl;
+            outputfile <<disk- 1 << " " << fromPin << " " << auxiliaryPin2<<endl;
+            outputfile <<disk<< " " << fromPin << " " << toPin<<endl;
+            outputfile <<disk- 1 << " " << auxiliaryPin2 << " " << toPin<<endl;
 
-            Hanoi4(disk - 2, auxiliaryPin1, fromPin, auxiliaryPin2, toPin);
+            Hanoi4(disk - 2,  outputfile, auxiliaryPin1, fromPin, auxiliaryPin2, toPin);
         }
        
     }
 }
 
-void Hanoi5(int disk, char fromPin, char auxiliaryPin1, char auxiliaryPin2, char auxiliaryPin3, char toPin) {
+void Hanoi5(int disk, ofstream& outputfile, char fromPin, char auxiliaryPin1, char auxiliaryPin2, char auxiliaryPin3, char toPin) {
     if (disk != 0) {
-        if (disk == 1)cout << disk << " " << fromPin << " " << toPin << endl;
+        if (disk == 1)outputfile << disk << " " << fromPin << " " << toPin << endl;
         else if (disk == 2) {
-            cout << disk-1 << " " << fromPin << " " << auxiliaryPin1 << endl;
-            cout << disk << " " << fromPin << " " << toPin << endl;
-            cout << disk-1 << " " << auxiliaryPin1 << " " << toPin << endl;
+            outputfile << disk-1 << " " << fromPin << " " << auxiliaryPin1 << endl;
+            outputfile << disk << " " << fromPin << " " << toPin << endl;
+            outputfile << disk-1 << " " << auxiliaryPin1 << " " << toPin << endl;
         }
         else {
-            Hanoi5(disk - 3, fromPin, auxiliaryPin2, auxiliaryPin3, toPin, auxiliaryPin1);
+            Hanoi5(disk - 3,  outputfile, fromPin, auxiliaryPin2, auxiliaryPin3, toPin, auxiliaryPin1);
 
-            cout << disk - 2 << " " << fromPin << " " << auxiliaryPin3 << endl;
-            cout << disk - 1 << " " << fromPin << " " << auxiliaryPin2 << endl;
-            cout << disk << " " << fromPin << " " << toPin << endl;
-            cout << disk - 1 << " " << auxiliaryPin2 << " " << toPin << endl;
-            cout << disk - 2 << " " << auxiliaryPin3 << " " << toPin << endl;
+            outputfile << disk - 2 << " " << fromPin << " " << auxiliaryPin3 << endl;
+            outputfile << disk - 1 << " " << fromPin << " " << auxiliaryPin2 << endl;
+            outputfile << disk << " " << fromPin << " " << toPin << endl;
+            outputfile << disk - 1 << " " << auxiliaryPin2 << " " << toPin << endl;
+            outputfile << disk - 2 << " " << auxiliaryPin3 << " " << toPin << endl;
 
-            Hanoi5(disk - 3, auxiliaryPin1, fromPin, auxiliaryPin2, auxiliaryPin3, toPin);
+            Hanoi5(disk - 3, outputfile, auxiliaryPin1, fromPin, auxiliaryPin2, auxiliaryPin3, toPin);
         }
 
     }
 }
 
-int main()
+
+int main(int argc, char** argv)
 {
-    int numPins;
-    int numDisks;
-   // Hanoi3(3, 'A', 'B', 'C');
-    Hanoi5(5,'A', 'B', 'C', 'D','E');
+    if (argc != 4)cout << "ERROR: INCORRECT NUMBER OF CCOMMAND LINE ARGUEMENTS\n";
+    else {
+        ofstream outputfile;
+        int numPins = strtol(argv[1], NULL, 10);
+        int numDisks = strtol(argv[2], NULL, 10);
+        string filename = argv[3];
+        outputfile.open(filename);
+        switch (numPins) {
+        case 3:
+            Hanoi3(numDisks,  outputfile, 'A', 'B', 'C');
+            break;
+        case 4:
+            Hanoi4(numDisks,  outputfile, 'A', 'B', 'C', 'D');
+            break;
+        case 5:
+            Hanoi5(numDisks,  outputfile, 'A', 'B', 'C','D','E');
+            break;
+        }
+        outputfile.close();
+    }
 }

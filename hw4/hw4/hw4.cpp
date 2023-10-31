60c962f1cde9ac82f3cbc99f8d8f6e2fc8c651ac
zacharyhillary <zacharyhillary1@gmail.com>
2023-10-31 02:09:46 -0500

    hw4almstodone
    hw4almstodone

--- hw4/hw4/hw4.cpp
+++ hw4/hw4/hw4.cpp
@@ -12,27 +12,49 @@ void Hanoi3(int disk, char fromPin, char auxiliaryPin, char toPin){
         Hanoi3(disk - 1, auxiliaryPin, fromPin, toPin);
     }
 }
-void Hanoi4(int n, char fromPin, char auxiliaryPin1, char auxiliaryPin2, char toPin) {
-    if (n != 0) {
-        if (n != 1)
-        {
-            Hanoi4(n - 2, fromPin, auxiliaryPin2, toPin, auxiliaryPin1);
+void Hanoi4(int disk, char fromPin, char auxiliaryPin1, char auxiliaryPin2, char toPin) {
+    if(disk!= 0) {
+        if(disk == 1) cout << disk << " " << fromPin << " " << toPin << endl;
+        else {
+            Hanoi4(disk - 2, fromPin, auxiliaryPin2, toPin, auxiliaryPin1);
 
-            cout << n - 1 << " " << fromPin << " " << auxiliaryPin2<<endl;
-            cout << n << " " << fromPin << " " << toPin<<endl;
-            cout << n - 1 << " " << auxiliaryPin2 << " " << toPin<<endl;
+            cout <<disk- 1 << " " << fromPin << " " << auxiliaryPin2<<endl;
+            cout <<disk<< " " << fromPin << " " << toPin<<endl;
+            cout <<disk- 1 << " " << auxiliaryPin2 << " " << toPin<<endl;
 
-            Hanoi4(n - 2, auxiliaryPin1, fromPin, auxiliaryPin2, toPin);
+            Hanoi4(disk - 2, auxiliaryPin1, fromPin, auxiliaryPin2, toPin);
         }
-        else cout << n << " " << fromPin << " " << toPin<<endl;
+       
     }
 }
 
+void Hanoi5(int disk, char fromPin, char auxiliaryPin1, char auxiliaryPin2, char auxiliaryPin3, char toPin) {
+    if (disk != 0) {
+        if (disk == 1)cout << disk << " " << fromPin << " " << toPin << endl;
+        else if (disk == 2) {
+            cout << disk-1 << " " << fromPin << " " << auxiliaryPin1 << endl;
+            cout << disk << " " << fromPin << " " << toPin << endl;
+            cout << disk-1 << " " << auxiliaryPin1 << " " << toPin << endl;
+        }
+        else {
+            Hanoi5(disk - 3, fromPin, auxiliaryPin2, auxiliaryPin3, toPin, auxiliaryPin1);
+
+            cout << disk - 2 << " " << fromPin << " " << auxiliaryPin3 << endl;
+            cout << disk - 1 << " " << fromPin << " " << auxiliaryPin2 << endl;
+            cout << disk << " " << fromPin << " " << toPin << endl;
+            cout << disk - 1 << " " << auxiliaryPin2 << " " << toPin << endl;
+            cout << disk - 2 << " " << auxiliaryPin3 << " " << toPin << endl;
+
+            Hanoi5(disk - 3, auxiliaryPin1, fromPin, auxiliaryPin2, auxiliaryPin3, toPin);
+        }
+
+    }
+}
 
 int main()
 {
     int numPins;
     int numDisks;
    // Hanoi3(3, 'A', 'B', 'C');
-    Hanoi4(3,'A', 'B', 'C', 'D');
+    Hanoi5(5,'A', 'B', 'C', 'D','E');
 }
